011fe177faa3195792ec12257792cd9492ef6d75
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-05-26 13:28:07 +0200

    Completed

---
+++ start.js
@@ -0,0 +1,22 @@
+///1
+const num1 = 6;
+///2
+const num2 = -13;
+///3
+const string1 = "this is a string";
+///4
+const string2 = `this is also a string`;
+///5
+const string3 = `this is an example of a variable "${string1}"`;
+///6
+let string4 = ``;
+///7
+let string5 = `\`` === '`';
+///8
+const string6 = `\`` === '`';
+///9
+console.log(typeof num2);
+///10
+console.log(typeof string1);
+///11
+console.log(typeof string6);
